5905987a6ac3307f8dc8aa80f8981d40a0b7ae03
alotanna <austineihejiben12@gmail.com>
2025-06-03 02:35:30 +0100

    Updated portfilio

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './portfolio.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <App />,
 )
