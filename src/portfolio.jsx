e8b8e160cd0541620412475c8a7dc956675db3b0
Austine Lotanna Iheji <145340711+alotanna@users.noreply.github.com>
2026-08-10 18:56:13 +0100

    add @vercel/analytics dependency and import in portfolio component

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -7,6 +7,8 @@ import samaCareImage from './assets/SamaCare.png';
 import kelenneImage from './assets/Kelenne.png';
 import eventManagerImage from './assets/EventManager.png';
 
+import { Analytics } from "@vercel/analytics/next"
+
 const Portfolio = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('home');
